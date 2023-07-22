7c00f0ab37a12c6400e0b1a46dc582bd596f82dc
Eva Kemp <eva.j.kemp@gmail.com>
2023-07-22 00:37:46 -0700

    wip

--- Bakery2/Models/Vendor.cs
+++ Bakery2/Models/Vendor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System;
 
 namespace Bakery2.Models
 {
@@ -46,6 +47,6 @@ public class Order
     public string OrderTitle { get; set; }
     public string OrderDescription { get; set; }
     public double Price { get; set; }
-    public int Date { get; set; }
+    public DateTime Date { get; set; }
 
 }
